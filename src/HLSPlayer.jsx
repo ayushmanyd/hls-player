6ab79ff44ba1bcbd82711cd4e7774fe424ac3da6
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-10-30 22:00:38 +0530

    HLS player added
    HLS player added

---
+++ src/HLSPlayer.jsx
@@ -0,0 +1,13 @@
+// src/HLSPlayer.js
+import React, { useEffect, useRef } from "react";
+import Hls from "hls.js";
+
+const HLSPlayer = ({ videoUrl }) => {
+  const videoRef = useRef(null);
+
+  return (
+    <div></div>
+  );
+};
+
+export default HLSPlayer;
