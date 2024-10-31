7d9afb6679e07cafdf74143d29389c5407a53971
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-10-31 20:06:06 +0530

    HLSPlayer.jsx updated
    HLSPlayer.jsx updated.

--- src/HLSPlayer.jsx
+++ src/HLSPlayer.jsx
@@ -5,6 +5,15 @@ import Hls from "hls.js";
 const HLSPlayer = ({ videoUrl }) => {
   const videoRef = useRef(null);
 
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (Hls.isSupported() && video) {
+      const hls = new Hls();
+
+      hls.loadSource(videoUrl);
+      hls.attachMedia(video);
+  }, [videoUrl]);
 
   return (
     <video ref={videoRef} controls className="w-full h-auto rounded-md mt-4">
