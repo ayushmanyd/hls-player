d0cd5df91066df5eef189b9a38641aa88321d2be
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-11-06 20:02:23 +0530

    Dark mode added
    Dark mode added.

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,12 @@ const App = () => {
 
   return (
     <div
-      >
+      className={`max-w-6xl mx-auto p-5 ${
+        isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+      }`}
+    >
+      <h1 className="text-4xl font-bold text-center mb-4">HLS Player</h1>
+      
     </div>
   );
 };
