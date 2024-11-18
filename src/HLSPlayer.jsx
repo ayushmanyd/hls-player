5141987a69950b0c61c988d97983bf17afd5dc1b
ayushmanyd <155382910+ayushmanyd@users.noreply.github.com>
2024-11-18 22:21:13 +0530

    HLSPlayer.jsx updated
    HLSPlayer.jsx updated for own media player controls.

--- src/HLSPlayer.jsx
+++ src/HLSPlayer.jsx
@@ -1,9 +1,23 @@
-// src/HLSPlayer.js
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Hls from "hls.js";
+import PlayIcon from "./assets/icons/play.svg";
+import PauseIcon from "./assets/icons/pause.svg";
+import BackwardIcon from "./assets/icons/backward.svg";
+import ForwardIcon from "./assets/icons/forward.svg";
+import VolumeMuteIcon from "./assets/icons/volume-mute.svg";
+import FullscreenExpandIcon from "./assets/icons/fullscreen-expand.svg";
+import FullscreenCompressIcon from "./assets/icons/fullscreen-compress.svg";
 
 const HLSPlayer = ({ videoUrl }) => {
   const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(1);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const [isDragging, setIsDragging] = useState(false); // Flag to track if the user is dragging the seekbar
+
 
   useEffect(() => {
     const video = videoRef.current;
